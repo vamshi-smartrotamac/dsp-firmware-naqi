1932bac453db5e11cbd21f7a4739ca4cfa7b224d
vamshi-smartrotamac <vamshi.b@smartrotamac.com>
2026-08-12 18:47:17 +0530

    ci: Update dsp-firmware main

--- src/system/main.c
+++ src/system/main.c
@@ -35,7 +35,7 @@
 int main(void)
 {
     /* Delay to prevent bricks */
-    for (volatile int idx = 0; idx < 0xFFFFFF; idx++)
+    for (volatile int i = 0; i < 0xFFFFFF; i++)
         ;
 
     Gpio_initialization();
